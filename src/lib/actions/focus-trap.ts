9041069295a5e42d745e111d2c370edc52f1bec6
Marian Meres <marian@meres.sk>
2024-01-02 23:18:36 +0100

    focus trap tweaks

--- src/lib/actions/focus-trap.ts
+++ src/lib/actions/focus-trap.ts
@@ -1,4 +1,4 @@
-// copied from skeleton
+// initially copied from skeleton, adjusted
 
 interface FocusTrapOptions {
 	enabled?: boolean;
@@ -7,23 +7,22 @@ interface FocusTrapOptions {
 
 const defaults: FocusTrapOptions = { enabled: true, autoFocusFirst: true };
 
-// Action: Focus Trap
 export function focusTrap(node: HTMLElement, options: FocusTrapOptions = {}) {
 	let { enabled, autoFocusFirst } = { ...defaults, ...(options || {}) };
 
 	const focusableSelectors = [
-		'[contentEditable=true]:not([tabindex^="-"])',
+		'[contentEditable=true]',
 		//
-		'button:not([disabled]):not([tabindex^="-"])',
-		'input:not([disabled]):not([tabindex^="-"])',
-		'select:not([disabled]):not([tabindex^="-"])',
-		'textarea:not([disabled]):not([tabindex^="-"])',
+		'button:not([disabled])',
+		'input:not([disabled])',
+		'select:not([disabled])',
+		'textarea:not([disabled])',
 		//
-		'a[href]:not([tabindex^="-"])',
-		'area[href]:not([tabindex^="-"])',
-		'details:not([tabindex^="-"])',
-		'iframe:not([tabindex^="-"])',
-		//
+		'a[href]',
+		'area[href]',
+		'details',
+		'iframe',
+		// see more below on tabindexes
 		'[tabindex]:not([tabindex^="-"])',
 	].join(',');
 
@@ -49,20 +48,26 @@ export function focusTrap(node: HTMLElement, options: FocusTrapOptions = {}) {
 	const queryElements = (fromObserver: boolean) => {
 		if (enabled === false) return;
 
-		const focusable: HTMLElement[] = (
+		let maxTabindex = 0;
+
+		let focusable: HTMLElement[] = (
 			[...node.querySelectorAll(focusableSelectors)] as HTMLElement[]
 		)
-			// in case I didn't get the selectors right, make sure to manually check as well...
-			// (negligible overhead, if any...)
+			// filter negative tabindexes (afaik there is no :not([disabled] OR [tabindex^="-"]))
 			.filter((e: HTMLElement) => {
+				// reusing loop for a side job here... see sort below
+				maxTabindex = Math.max(maxTabindex, parseInt(e.getAttribute('tabindex') || '0'));
+				//
 				if (e.getAttribute('disabled') === '') return false;
 				if ((e.getAttribute('tabindex') || '').startsWith('-')) return false;
 				return true;
 			})
 			// important to sort by tabindex, so the first/last will work as expected
+			// but must increase zero to max + 1 first, because browsers focus zeros as last...
+			// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/tabindex
 			.sort((e1: HTMLElement, e2: HTMLElement) => {
-				const a = parseInt(e1.getAttribute('tabindex') || '0');
-				const b = parseInt(e2.getAttribute('tabindex') || '0');
+				const a = parseInt(e1.getAttribute('tabindex') || '0') || maxTabindex + 1;
+				const b = parseInt(e2.getAttribute('tabindex') || '0') || maxTabindex + 1;
 				return a - b;
 			});
 
@@ -82,22 +87,20 @@ export function focusTrap(node: HTMLElement, options: FocusTrapOptions = {}) {
 	queryElements(false);
 
 	function cleanup(): void {
-		if (first) first.removeEventListener('keydown', onFirstElemKeydown);
-		if (last) last.removeEventListener('keydown', onLastElemKeydown);
+		first && first.removeEventListener('keydown', onFirstElemKeydown);
+		last && last.removeEventListener('keydown', onLastElemKeydown);
 	}
 
 	// When children of node are changed (added or removed)
-	const onObservationChange = (
-		mutationRecords: MutationRecord[],
-		observer: MutationObserver
-	) => {
-		if (mutationRecords.length) {
-			cleanup();
-			queryElements(true);
+	const observer = new MutationObserver(
+		(mutations: MutationRecord[], observer: MutationObserver) => {
+			if (mutations.length) {
+				cleanup();
+				queryElements(true);
+			}
+			return observer;
 		}
-		return observer;
-	};
-	const observer = new MutationObserver(onObservationChange);
+	);
 	observer.observe(node, { childList: true, subtree: true });
 
 	// Lifecycle
