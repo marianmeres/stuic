cfd8ab044c683759ea3f535bd76bdac13fbb9197
Marian Meres <marian@meres.sk>
2024-08-13 09:19:04 +0200

    docs

--- src/lib/utils/tw-merge2.ts
+++ src/lib/utils/tw-merge2.ts
@@ -2,6 +2,6 @@ import { twMerge } from 'tailwind-merge';
 
 export const clsClean = (s: string) => `${s || ''}`.replace(/\s+/gi, ' ').trim();
 
-// twMerge does not seem to handle "\t" and/or "\n" within the input strings correctly,
+// twMerge does not seem to handle "\r", "\n" and/or "\t" within the input strings correctly,
 // so we need to do the cleanup ourselves
 export const twMerge2 = (...args: any[]) => twMerge(...args.map(clsClean));
