a6d923a395e2fd40295c80f21735bcd065f4d9b3
Marian Meres <marian@meres.sk>
2025-11-01 11:16:13 +0100

    Drawer noBackropScrollLock

--- src/lib/components/Drawer/Drawer.svelte
+++ src/lib/components/Drawer/Drawer.svelte
@@ -30,6 +30,7 @@
 		//
 		onOutside?: () => void;
 		// onOutsideEnabled?: boolean;
+		noBackropScrollLock?: boolean;
 	}
 
 	let {
@@ -48,6 +49,7 @@
 		animOffset = "75vw",
 		onEscape,
 		onOutside,
+		noBackropScrollLock,
 		// onOutsideEnabled = true,
 	}: Props = $props();
 
@@ -136,6 +138,7 @@
 	fadeOutDuration={transitionDuration}
 	{onEscape}
 	onmousedown={() => onOutside?.()}
+	noScrollLock={noBackropScrollLock}
 >
 	<!-- svelte-ignore a11y_interactive_supports_focus -->
 	<!-- svelte-ignore a11y_click_events_have_key_events -->
